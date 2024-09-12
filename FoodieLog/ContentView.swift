7db73b372430da470fdda5d62ec44eaaa95d3bf5
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-12 21:49:54 +0900

    Initial Commit

---
+++ FoodieLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodieLog/FoodieLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodieLogApp.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FoodieLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
