311e9b77991dc10e5d728c4ffa64f480aa2ca011
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-12 22:22:12 +0900

    🎉 init : Initial Commit

---
+++ FoodieLog/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/12/24.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
