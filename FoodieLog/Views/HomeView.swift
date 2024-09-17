e34d70a1bf14388e97c9aae0b75e73f8f6791528
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-17 19:50:46 +0900

    🐛 fix : HomeView 수정

--- FoodieLog/Views/HomeView.swift
+++ FoodieLog/Views/HomeView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                HStack {
+                    Text("최근 방문한 맛집")
+                    Spacer()
+                }
+                .font(.title2)
+                .bold()
+                .padding(.leading, 20)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack {
+                        ForEach(0..<5) { _ in
+                            FoodCardView()
+                        }
+                    }
+                }
+                .padding(.top, -30)
+            }
+            .navigationTitle("맛집 Log")
+        }
+        
     }
 }
 
