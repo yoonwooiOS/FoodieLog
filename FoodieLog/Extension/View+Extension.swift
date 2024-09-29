0b6819e086aebfe22a401078e464a331ab1415e4
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-29 23:29:38 +0900

    ✨ feat : DateFormatter 추가

--- FoodieLog/Extension/View+Extension.swift
+++ FoodieLog/Extension/View+Extension.swift
@@ -8,7 +8,14 @@
 import SwiftUI
 
 extension View {
-  func hideKeyboard() {
-    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
-  }
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+    func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy년 MM월 dd일"
+        formatter.locale = Locale(identifier: String(Locale.preferredLanguages[0]))
+        return formatter.string(from: date)
+    }
+    
 }
