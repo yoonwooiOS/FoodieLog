e09810777460573bfabd259c63d72d66dc750398
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-30 23:38:26 +0900

    ✨ feat : ReviewTable, ReviewRepository 패턴 추가

---
+++ FoodieLog/DB/Review.swift
@@ -0,0 +1,38 @@
+//
+//  Review.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/29/24.
+//
+
+import RealmSwift
+import Foundation
+
+class Review: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var title: String
+    @Persisted var rating: Double
+    @Persisted var date: Date
+    @Persisted var content: String
+    @Persisted var restaurantName: String
+    @Persisted var restaurantAddress: String
+    @Persisted var latitude: String
+    @Persisted var longitude: String
+    @Persisted var imagePaths: List<String>
+    @Persisted var category: String?
+    
+    convenience init(title: String, rating: Double, date: Date, content: String, restaurantName: String, restaurantAddress: String, latitude: String, longitude: String, imagePaths: [String], category: String? = nil) {
+        self.init() // 지정된 초기화 메서드를 호출
+        self.title = title
+        self.rating = rating
+        self.date = date
+        self.content = content
+        self.restaurantName = restaurantName
+        self.restaurantAddress = restaurantAddress
+        self.latitude = latitude
+        self.longitude = longitude
+        self.imagePaths.append(objectsIn: imagePaths)
+        self.category = category
+    }
+}
+
---
+++ FoodieLog/DB/ReviewPostRepository.swift
@@ -0,0 +1,108 @@
+//
+//  ReviewPostRepository.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/30/24.
+//
+
+import Foundation
+import RealmSwift
+import UIKit
+
+final class ReviewRepository {
+    private let realm = try! Realm()
+    
+    // CRUD operations
+    func add(_ review: Review) {
+        do {
+            try realm.write {
+                realm.add(review)
+                print("Review added successfully.")
+            }
+        } catch {
+            print("Error adding Review: \(error)")
+        }
+    }
+    
+    func fetchAll() -> [Review] {
+        let reviews = realm.objects(Review.self)
+            .sorted(byKeyPath: "date", ascending: false)
+        return Array(reviews)
+    }
+    
+    func fetch(by id: ObjectId) -> Review? {
+        return realm.object(ofType: Review.self, forPrimaryKey: id)
+    }
+    
+    func update(_ review: Review, with newValues: Review) {
+        do {
+            try realm.write {
+                review.title = newValues.title
+                review.rating = newValues.rating
+                review.date = newValues.date
+                review.content = newValues.content
+                review.restaurantName = newValues.restaurantName
+                review.restaurantAddress = newValues.restaurantAddress
+                review.latitude = newValues.latitude
+                review.longitude = newValues.longitude
+                
+                realm.add(review, update: .modified)
+                print("Review updated successfully.")
+            }
+        } catch {
+            print("Error updating Review: \(error)")
+        }
+    }
+    
+    func delete(_ review: Review) {
+        do {
+            try realm.write {
+                realm.delete(review)
+                print("Review deleted successfully.")
+            }
+        } catch {
+            print("Error deleting Review: \(error)")
+        }
+    }
+    
+    func deleteAll() {
+        do {
+            try realm.write {
+                let allReviews = realm.objects(Review.self)
+                realm.delete(allReviews)
+                print("All Reviews deleted successfully.")
+            }
+        } catch {
+            print("Error deleting all Reviews: \(error)")
+        }
+    }
+    
+    func saveReviewWithImages(review: Review, images: [UIImage]) {
+        var imagePaths: [String] = []
+        
+        for (index, image) in images.enumerated() {
+            if let imageName = ImageManager.shared.saveImageToDisk(image: image, imageName: "review_image_\(index)_\(review.id.stringValue)") {
+                imagePaths.append(imageName)
+            }
+        }
+        
+        do {
+            try realm.write {
+                review.imagePaths.append(objectsIn: imagePaths)
+                realm.add(review)
+            }
+        } catch {
+            print("Error saving review with images: \(error)")
+        }
+    }
+    func fetchLatestFive() -> [Review] {
+          let reviews = realm.objects(Review.self)
+              .sorted(byKeyPath: "date", ascending: false)
+              .prefix(5)
+        print(reviews)
+          return Array(reviews)
+      }
+    func detectRealmURL() {
+        print("Realm file path: \(realm.configuration.fileURL?.absoluteString ?? "No file URL")")
+    }
+}
