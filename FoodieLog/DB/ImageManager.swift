8508da3fb24aa655f162e88808269b81897129de
yoonwooiOS <yoonwoo4429@kakao.com>
2024-09-30 23:36:09 +0900

    ✨ feat : ImageManager 기능 구현

---
+++ FoodieLog/DB/ImageManager.swift
@@ -0,0 +1,50 @@
+//
+//  ImageManager.swift
+//  FoodieLog
+//
+//  Created by 김윤우 on 9/30/24.
+//
+
+import UIKit
+
+class ImageManager {
+    static let shared = ImageManager()
+    
+    private init() {}
+    
+    func saveImageToDisk(image: UIImage, imageName: String) -> String? {
+        if let data = image.jpegData(compressionQuality: 0.8) {
+            let fileName = "\(imageName).jpg"
+            let path = getDocumentsDirectory().appendingPathComponent(fileName)
+            do {
+                try data.write(to: path)
+                return fileName
+            } catch {
+                print("Failed to save image: \(error)")
+                return nil
+            }
+        }
+        return nil
+    }
+
+    func loadImageFromDisk(imageName: String) -> UIImage? {
+        let path = getDocumentsDirectory().appendingPathComponent(imageName)
+        return UIImage(contentsOfFile: path.path)
+    }
+    func loadImagesFromDisk(imageNames: [String]) -> [UIImage] {
+            var images: [UIImage] = []
+            
+            for imageName in imageNames {
+                if let image = loadImageFromDisk(imageName: imageName) {
+                    images.append(image)
+                } else {
+                    print("Failed to load image: \(imageName)")
+                }
+            }
+            
+            return images
+        }
+    private func getDocumentsDirectory() -> URL {
+        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+    }
+}
