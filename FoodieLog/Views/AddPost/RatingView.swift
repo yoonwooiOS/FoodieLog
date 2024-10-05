830b4e1d30747e3594224455bf55f7f82d5014a3
yoonwooiOS <yoonwoo4429@kakao.com>
2024-10-05 21:48:35 +0900

    🐛 fix : 모든 뷰에서 별점 수정되는 오류 수정

--- FoodieLog/Views/AddPost/RatingView.swift
+++ FoodieLog/Views/AddPost/RatingView.swift
@@ -11,19 +11,26 @@ import Cosmos
 struct RatingView: UIViewRepresentable {
     @Binding var rating: Double
     var starSize: Double?
+    var isInteractive: Bool 
+
     func makeUIView(context: Context) -> CosmosView {
         let cosmosView = CosmosView()
         cosmosView.settings.fillMode = .half
         cosmosView.settings.starSize = starSize ?? 40
-        cosmosView.settings.updateOnTouch = true
-        cosmosView.settings.minTouchRating = 0 
-        cosmosView.didFinishTouchingCosmos = { value in
-            rating = value
+        cosmosView.settings.updateOnTouch = isInteractive
+        cosmosView.settings.minTouchRating = 0
+
+        if isInteractive {
+            cosmosView.didFinishTouchingCosmos = { value in
+                rating = value
+            }
         }
+
         return cosmosView
     }
-    
+
     func updateUIView(_ uiView: CosmosView, context: Context) {
         uiView.rating = rating
+        uiView.settings.updateOnTouch = isInteractive
     }
 }
