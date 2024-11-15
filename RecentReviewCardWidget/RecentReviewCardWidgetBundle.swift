79e76aa266f864bf08db3262bfa4a9204a7b8445
yoonwooiOS <yoonwoo4429@kakao.com>
2024-11-15 20:50:35 +0900

    ✨ Feat : widget 화면 및 기능 구현

---
+++ RecentReviewCardWidget/RecentReviewCardWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  RecentReviewCardWidgetBundle.swift
+//  RecentReviewCardWidget
+//
+//  Created by 김윤우 on 11/15/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct RecentReviewCardWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        RecentReviewCardWidget()
+    }
+}
